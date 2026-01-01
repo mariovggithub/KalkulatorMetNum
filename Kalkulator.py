dd3da0b28b017461336ddc0c3686f35074b70d30
Marco Alexander Tejo <143802372+marco12893@users.noreply.github.com>
2026-01-01 20:34:12 +0700

    coba coba

--- Kalkulator.py
+++ Kalkulator.py
@@ -3,7 +3,7 @@ import math
 
 # FUNCTION TABEL
 def print_table(headers, rows):
-    col_widths = [len(str(h)) for h in headers]  # coba coba
+    col_widths =  [len(str(h)) for h in headers]  # coba coba
 
     for row in rows:
         for i, item in enumerate(row):
